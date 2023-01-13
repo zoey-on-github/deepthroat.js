a40bba81d3967e1ca00a4ec7ff272704759f1492
codelikecrazy <codelikecraze@gmail.com>
2023-01-13 10:11:28 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 module.exports = function deepthroat(object) {
-if (typeof object !== "object") throw new TypeError ("object to deepthroat needed");
-    return JSON.parse(JSON.stringify(object));
+    if (typeof object == "number" || object == null || object == true || object == false || object instanceof Function) {
+        return object;
+    }
+    var output = new object.constructor();
+    for (var i in object) {
+        output[i] = deepthroat(object[i]);
+    }
+    return output;
 }
